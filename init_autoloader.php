5d1e924ab9155fa4e1a68a624b7912cddc27edc5
heena1234 <heena.aggarwal@gmail.com>
2017-03-22 11:58:44 +0530

    my changes

--- init_autoloader.php
+++ init_autoloader.php
@@ -18,7 +18,7 @@
 
 // Composer autoloading
 if (file_exists('vendor/autoload.php')) {
-    $loader = include 'vendor/autoload.php';
+    $loader = include 'vendor/autoload.php';  kjdbckdjbckdbcldkbclkdcl jbcjcbjkdbc
 }
 
 $zf2Path = false;
